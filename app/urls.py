5f6bb1cb06a3f9ee760d4312908101cc79941640
Atticus Walton <atticus.falkner.walton@gmial.com>
2025-11-18 20:56:41 +0000

    removed account urls from app

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,4 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('signup/', views.register, name='register'),
 ]
