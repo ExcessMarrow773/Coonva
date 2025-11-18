1fb6a093a459549000fa7ff18a438f59e8061f4f
Atticus Walton <atticus.falkner.walton@gmial.com>
2025-11-18 20:56:54 +0000

    removed account views from app

--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth import get_user_model, login
 from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-from app.forms import CustomAuthenticationForm, CustomUserCreationForm
 
 
 from itertools import chain
@@ -18,29 +17,7 @@ User = get_user_model()
 # Create your views here.
 
 def index(request):
-    context = {
-        
-    }
-    return render(request, 'app/index.html', context)
+	context = {
+	}
+	return render(request, 'app/index.html', context)
 
-@csrf_exempt
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()         # important — saves hashed password
-            login(request, user)       # optional: log user in immediately
-            return redirect('app:index')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'register.html', {'form': form})
-
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
-    authentication_form = CustomAuthenticationForm
-
-class CustomLogoutView(LogoutView):
-    template_name = 'index.html'
-
-def handler405(request, exception=None):
-	return render(request, '405.html', status=405)
