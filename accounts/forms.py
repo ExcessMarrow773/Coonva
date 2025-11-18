513b63de94e07f09b510af91720d1b8c58690ee3
Atticus Walton <atticus.falkner.walton@gmial.com>
2025-11-18 20:55:17 +0000

    moves UserFormsto accounts

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.admin.widgets import AdminSplitDateTime
+from django.utils import timezone
+from accounts.models import CustomUser
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "password1", "password2")
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass
+
+class ProfileImageChange(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['profile_image']
