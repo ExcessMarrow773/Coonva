f5145494d30f653798c21bb479b4a4940770c237
Atticus Walton <atticus.falkner.walton@gmail.com>
2025-11-20 20:50:58 +0000

    adds a better passwordchange form

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,19 +3,27 @@ from django.contrib.admin.widgets import AdminSplitDateTime
 from django.utils import timezone
 from accounts.models import CustomUser
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 
 User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "password1", "password2")
+        fields = ("username", "email", "password1", "password2")
 
 class CustomAuthenticationForm(AuthenticationForm):
     pass
 
-class ProfileImageChange(forms.ModelForm):
+class ProfileDetailsChange(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['profile_image']
+        fields = ['profile_image', 'first_name', 'last_name', 'email']
+
+class ProfileUsernameChange(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username']
+
+class ProfilePasswordChange(PasswordChangeForm):
+    user = User
