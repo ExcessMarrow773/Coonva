85ac38ed76cdde28220d361d659d4abe84a9ebd1
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-12-22 22:43:30 -0600

    adds email field

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 	profile_image = models.ImageField(upload_to='pfps/', default='static/img/pfp.png')
 
+	# make email unique because it's used as the USERNAME_FIELD
+	email = models.EmailField(max_length=254, unique=True)
 
 	USERNAME_FIELD = 'email'
 	REQUIRED_FIELDS = ['username']
