9eb8123150ab50faee559d24b988447be686b9da
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-12-22 22:43:09 -0600

    removes following/ followers

--- accounts/models.py
+++ accounts/models.py
@@ -4,22 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-	following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
-
 	profile_image = models.ImageField(upload_to='pfps/', default='static/img/pfp.png')
 
 
-	def follow(self, user):
-		self.following.add(user)
-
-	def unfollow(self, user):
-		self.following.remove(user)
-
-	def follower_count(self):
-		return len(self.followers.all())
-
-	def is_following(self, user):
-		return self.following.filter(pk=user.pk).exists()
-
-	def is_followed_by(self, user):
-		return self.followers.filter(pk=user.pk).exists()
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
