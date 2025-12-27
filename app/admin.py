711af4b00a5b3980b684bea2dbb7226de0eeb667
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-12-27 01:21:51 -0600

    adds admin pages for Classes, Assignments, and Submissions

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from app.models import Class, Assignment, Submission
 
 # Register your models here.
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+	list_display = ('title', 'student_count')
+	list_filter = ('created_on', )
+	date_hierarchy = 'created_on'
+	search_fields = ('title', 'teachers')
+
+	def student_count(self, obj):
+		return obj.students.count()
+	student_count.short_description = 'Student Count'
+
+	fieldsets = [
+		(None, {
+			'fields': ('title', 'teachers', 'students', 'assignments')
+			}
+		)
+	]
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+	list_display = ('name', 'created_on', 'due_at', 'locks_at')
+	list_filter = ('due_at', 'locks_at', 'created_on')
+	date_hierarchy = 'due_at'
+	search_fields =['name']
+
+	def assignment_class(self, obj):
+		return obj.entry_set.all()
+
+	fieldsets = [
+		(None, {
+			'fields': ('name', 'details')
+			}
+		),
+		('Dates', {
+			'fields': ('due_at', 'created_on', 'locks_at')
+			}
+		)]
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+	list_display = ('student', 'created_on')
+	list_filter = ['created_on']
+	date_hierarchy = 'created_on'
+	search_fields = ['student']
