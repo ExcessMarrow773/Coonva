3b54a5dba09d5847598e51475a799888c84d3f11
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2026-01-07 01:48:10 -0600

    docker things

--- coonva/settings.py
+++ coonva/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'oi01p)*&Ybnl908oh72qjikdn-9q8e-cqijk;dkmnqwq3)(*&)*(UYH jbn087y087
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+docker = True
 ALLOWED_HOSTS = ['192.168.1.199', '127.0.0.1', '192.168.3.146', '0.0.0.0', 'localhost']
 
 
@@ -74,12 +74,26 @@ WSGI_APPLICATION = 'coonva.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+if docker == True:
+	DATABASES = {
+	        "default": {
+	        "ENGINE": "django.db.backends.postgresql",
+	        "NAME": "postgres",
+	        "USER": "postgres",
+	        "PASSWORD": "postgres",
+	        "HOST": "db",  # set in docker-compose.yml
+	        "PORT": 5432,  # default postgres port
+	        }
 	}
-}
+else:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': BASE_DIR / 'db.sqlite3',
+		}
+	}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
